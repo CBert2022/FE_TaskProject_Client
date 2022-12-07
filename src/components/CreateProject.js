412855023a2e90e8ebc250af65ca742ae1cadb11
Angela Salazar <angelasalazargarcia04@gmail.com>
2022-12-07 17:54:11 +0100

    little change in the API_URL for DB or local

--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
  
-const API_URL = "http://localhost:5005";
+const API_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:5005"
  
 function CreateProject({refresh}) {
   const [title, setTitle] = useState("");
