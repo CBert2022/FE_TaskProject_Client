1974f756b8f88f59165bf7c28a2d68429c38c190
Carolin Rieche <Ccarolin.rieche@yahoo.de>
2022-12-07 11:14:31 +0100

    connect FE and BE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom"; 
+ 
+import Navbar from "./components/Navbar";     
+import ProjectList from "./components/ProjectList";     
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+ 
+      <Routes>      
+        <Route path="/" element={ <ProjectList /> } />
+    
+      </Routes>
+      
     </div>
   );
 }
-
+ 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+
+ 
+function Navbar() {
+  return (
+    <nav>
+
+ 
+        <button>Dark/Ligth Mode</button>
+        <button>Language Switch</button>
+  
+    </nav>
+  );
+}
+ 
+export default Navbar;
