1ac7ff5b63b8fba4d20a09a91d8e0fa252d9552d
Angela Salazar <angelasalazargarcia04@gmail.com>
2022-12-12 10:17:50 +0100

    draggable projects bug fixed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,30 +4,29 @@ import CreateTask from "./CreateTask";
 import QuickEntryTask from "./QuickEntryTask";
 import { Link } from "react-router-dom";
 import TaskListPage from "./TaskList";
- 
+
 const API_URL = "http://localhost:5005"
 
-function ProjectListPage({projects, setProjects, getAllProjects, deleteTask}) {
+function ProjectListPage({ projects, setProjects, getAllProjects, deleteTask }) {
   // console.log("PROJECTS: ", projects)
 
   const [isShown, setIsShown] = useState("")
-   const dragItem = useRef();
+  const dragItem = useRef();
   const dragOverItem = useRef();
 
 
 
-  const updateList =() => {
-    console.log("HELLOO")
-  projects && setTimeout(()=> {
-    axios
-    .post(`${API_URL}/api/projects/sort`, {array: projects})
-  },1000)
-    };
-  
+  const updateList = () => {
+    /* projects && setTimeout(() => { */
+      axios
+        .post(`${API_URL}/api/projects/sort`, { array: copyListItems })
+    /* }, 10) */
+  };
 
 
- const dragStart = (element, position) => {
-  console.log("PROjECTS: ", projects)
+
+  const dragStart = (element, position) => {
+    console.log("PROjECTS: ", projects)
     dragItem.current = position;
     // console.log("DRAG START ",element.target);
   };
@@ -38,11 +37,11 @@ function ProjectListPage({projects, setProjects, getAllProjects, deleteTask}) {
     // console.log("DRAG END ",element.target);
   };
 
-
+  let copyListItems = []
 
   const drop = () => {
     // console.log("PROJECTS3", projects)
-    let copyListItems = [...projects]
+    copyListItems = [...projects]
     // const copyListItems = JSON.parse(JSON.stringify(projects));
     // console.log("COPY LIST ITEMS: ", copyListItems)
     // console.log("DRAG ITEM", dragItem);
@@ -53,68 +52,70 @@ function ProjectListPage({projects, setProjects, getAllProjects, deleteTask}) {
     dragItem.current = null;
     dragOverItem.current = null;
     console.log("LIST: ", copyListItems)
-    setProjects(copyListItems);
-      updateList()
-    
+    setProjects(copyListItems)
+    console.log(copyListItems)
+    updateList()
+
   };
- 
+
   const deleteProject = (id) => {
-    axios 
-    .post(`${API_URL}/api/projects/${id}/delete`)
-    .then(() => getAllProjects())
-    .catch((error) => console.log(error));
+    axios
+      .post(`${API_URL}/api/projects/${id}/delete`)
+      .then(() => getAllProjects())
+      .catch((error) => console.log(error));
   }
 
 
   useEffect(() => {
     getAllProjects();
-  }, [] );
-    
+  }, []);
+
   return (
     <div className="ProjectListPage">
-        
+
       {projects?.map((project, i) => {
         return (
           <>
 
-<div>
-            <div className="ProjectCard card" 
-            key={project._id} 
-            onDragStart={(elem) => dragStart(elem, i)}
-            onDragEnter={(elem) => dragEnter(elem, i)} 
-            onDragEnd={drop}
-            draggable >
-              
-              <Link onClick={() => setIsShown(project?._id)}> 
-                <h3>{project?.title}</h3>
-              </Link>
-              <button onClick={()=>deleteProject(project?._id)}  > Delete </button> 
-            </div>
             <div>
+              <div className="ProjectCard card"
+                key={i}
+                onDragStart={(elem) => dragStart(elem, i)}
+                onDragEnter={(elem) => dragEnter(elem, i)}
+                onDragEnd={drop}
+                draggable >
 
-              <TaskListPage 
-              project={project} 
-              isShown={isShown} 
-              deleteTask={deleteTask} 
-              dragStart={dragStart}
-              dragEnter={dragEnter}
-              drop={drop} 
-              setProjects={setProjects}/>
-               
+                <Link onClick={() => setIsShown(project?._id)}>
+                  <h3>{project?.title}</h3>
+                </Link>
+                <button onClick={() => deleteProject(project?._id)}  > Delete </button>
+              </div>
+              <div>
+
+                <TaskListPage
+                  project={project}
+                  isShown={isShown}
+                  deleteTask={deleteTask}
+                  dragStart={dragStart}
+                  dragEnter={dragEnter}
+                  drop={drop}
+                  setProjects={setProjects} />
+
+              </div>
             </div>
-</div>
-{/* 
+            {/* 
             {isShown === project._id && <div><QuickEntryTask projectId={project._id} refresh={getAllProjects}/></div>}
             {isShown === project._id && <div><CreateTask projectId={project._id} refresh={getAllProjects}/></div> }
  */}
+
           </>
         );
-      })}  
+      })}
 
 
 
     </div>
   );
 }
-   
-  export default ProjectListPage;
+
+export default ProjectListPage;
