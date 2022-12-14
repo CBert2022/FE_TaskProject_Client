75f19ffde01ed96c9b4bdec2f24f6151c206df7f
Carolin Rieche <Ccarolin.rieche@yahoo.de>
2022-12-14 17:10:54 +0100

    transition try, due date tranfor

---
+++ src/components/FilteredTasks.js
@@ -0,0 +1,25 @@
+
+
+
+
+// function FilteredTasks({ScheduledTask}) {
+
+
+//     return (
+//         <>
+//             <div className="ImportantCard card" onClick={() => {
+        
+//                   }}>
+//                 <h3>Important Tasks</h3>
+//             </div>
+//             <div  className="ScheduledCard card" onClick={() => {getSpecificTasks(taskId)}}>
+//                 <h3>Scheduled Tasks</h3>
+//             </div>
+//         </>
+//     )
+
+
+
+// }
+
+// export default FilteredTasks;
