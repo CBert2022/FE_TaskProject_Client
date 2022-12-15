019217630c7bf9208c0d9d146fecafffc513d9bf
Carolin Rieche <Ccarolin.rieche@yahoo.de>
2022-12-15 19:17:50 +0100

    test

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:5005"
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005"
 
 const AuthContext = React.createContext();
 
